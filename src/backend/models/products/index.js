a8a25672815e59e831c469a86f69dd3dbd1dd23b
dule012 <dusanmihajlovic012@gmail.com>
2023-06-09 16:57:21 +0200

    init products docs

--- src/backend/models/products/index.js
+++ src/backend/models/products/index.js
@@ -11,6 +11,18 @@ const schema = new mongoose.Schema({
   },
 });
 
-export default mongoose.models.Products
+const Products = mongoose.models.Products
   ? mongoose.model("Products")
   : mongoose.model("Products", schema);
+
+Products.find({}).then(
+  (docs) =>
+    !docs.length &&
+    Products.insertMany([
+      new Products({ name: "Product1", price: 90 }),
+      new Products({ name: "Product2", price: 80 }),
+      new Products({ name: "Product3", price: 70 }),
+    ])
+);
+
+export default Products;
